d2c2082ec75dd14528417b54ac00f0e7cd13c884
zhangnaiyuan <zhangnaiyuan@ccrmanager.com>
2023-02-22 19:48:08 +0800

    bug fix on jwt authentication

--- demo/src/main/java/com/lionsbot/demo/config/JwtAuthenticationFilter.java
+++ demo/src/main/java/com/lionsbot/demo/config/JwtAuthenticationFilter.java
@@ -42,7 +42,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 		final String username = jwtService.extractUsername(jwt);
 		if(username!=null && SecurityContextHolder.getContext().getAuthentication() == null) {
 			UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
-			if(jwtService.isTokenValid(username, userDetails)) {
+			if(jwtService.isTokenValid(jwt, userDetails)) {
 				UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
 						userDetails,
 						null,
