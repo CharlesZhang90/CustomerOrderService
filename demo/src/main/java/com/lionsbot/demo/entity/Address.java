88f2ae495312674c02aabfa4a590d560268bb73b
zhangnaiyuan <zhangnaiyuan@ccrmanager.com>
2023-02-23 14:09:04 +0800

    End commit

--- demo/src/main/java/com/lionsbot/demo/entity/Address.java
+++ demo/src/main/java/com/lionsbot/demo/entity/Address.java
@@ -9,7 +9,6 @@ import com.lionsbot.demo.dto.CustomerDTO;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
